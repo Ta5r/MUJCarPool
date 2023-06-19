7227f7f33f0d705f950adcd723c27ed5a1b028de
Tanay Srivastava <srv.tanay@gmail.com>
2023-06-20 01:27:48 +0530

    feat : Skeleton Loading for MyRequest status response

--- user/src/pages/MyRequests/index.jsx
+++ user/src/pages/MyRequests/index.jsx
@@ -3,18 +3,22 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/User/Navbar';
 import { Box, Text, ChakraProvider, theme } from '@chakra-ui/react';
 import RideCard from '../../components/User/MyRequests';
+import LoadingCard from '../../components/layouts/LoadingCard';
 
 const RidesSearch = () => {
   const [myRequests, setMyRequests] = useState([]);
   const UID = parseInt(localStorage.getItem('UID'));
+  const [loading, setLoad] = useState(false);
 
   useEffect(() => {
     try {
+      setLoad(true);
       axios
         .get(
           `https://muj-travel-buddy.onrender.com/requests?requestee_id=${UID}`
         )
         .then(response => {
+          setLoad(false);
           setMyRequests(response.data);
         });
     } catch (err) {
@@ -30,6 +34,9 @@ const RidesSearch = () => {
         <Text fontWeight={'bold'} fontSize="38px" my="4rem" mx="5rem">
           My Requests Status
         </Text>
+        {(loading===true)?
+      <LoadingCard/>
+      :null}
         {myRequests.map(res => (
           <RideCard
             key={`${res.ride_id}-${res.publisher_id}`}
