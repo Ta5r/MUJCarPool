b5bdc9f93b4ce24a81b6b727818591d6c015e894
Tanay Srivastava <srv.tanay@gmail.com>
2023-06-20 01:33:09 +0530

    feat : Skeleton Loading for MyOngoing Rides response

--- user/src/pages/MyRides/index.jsx
+++ user/src/pages/MyRides/index.jsx
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/User/Navbar';
 import { Text, ChakraProvider,Box, theme } from '@chakra-ui/react';
 import MyRide from '../../components/User/MyRide';
+import LoadingCard from '../../components/layouts/LoadingCard';
 
 const MyRides = () => {
   const [allRides, setAllRides] = useState([]);
   const UID = localStorage.getItem('UID');
+  const [loading, setLoad] = useState(false);
 
   useEffect(() => {
     try {
+      setLoad(true);
       axios.get(`https://muj-travel-buddy.onrender.com/users/${UID}/rides`).then(response => {
+        setLoad(false);  
         setAllRides(response.data);
       });
     } catch (err) {
@@ -26,6 +30,10 @@ const MyRides = () => {
       <Text fontWeight={'bold'} fontSize="38px" my="4rem" mx="5rem">
         My Ongoing Rides
       </Text>
+
+      {(loading===true)?
+      <LoadingCard/>
+      :<p>Looks like you have not published any rides.</p>}
 
       {allRides.map(res => {
         return (
