e765a5cac1a17311413f6f9848f06ef0d5057b81
Tanay Srivastava <srv.tanay@gmail.com>
2023-06-23 01:03:39 +0530

    Redesign Loading Card

--- user/src/components/layouts/LoadingCard.jsx
+++ user/src/components/layouts/LoadingCard.jsx
@@ -1,59 +1,54 @@
 import React from 'react';
-import Card from '../layouts/Card';
-import {  SimpleGrid, Box,Skeleton, SkeletonText  } from '@chakra-ui/react';
+import { Skeleton, SkeletonText, SkeletonCircle, Text, Box, HStack, VStack, Flex, Spacer } from '@chakra-ui/react';
 
 const LoadingCard = () => {
   return (
-      <Card
-        py="3rem"
-        my="2rem"
-        px="2rem"
-        bg={'white'}
-        position="relative"
-        mx={['1rem', '2rem', '3rem', '4rem']}
-        width="80vw"
-        borderRadius="16px"
-        boxShadow=" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-        height={{ xs: '400px', sm: '300px', md: '150px', lg: '150px' }}
-      >
-        <SimpleGrid columns={[1, 3, 4, 5, 6,7]} spacing="40px">
-          <Box
-            w="100%"
-            textAlign={'center'}
-            verticalAlign={'middle'}
-            h={'60px'}
-          >
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
-           
-          </Box>
-          <Box
-            w="100%"
-            textAlign={'center'}
-            verticalAlign={'middle'}
-            h={'60px'}
-          >
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
-          </Box>
+    <Flex
+      minW={'340px'}
+      bgColor={'white'}
+      p={'2rem'}
+      mx={'1rem'}
+      my={'2rem'}
+      borderRadius="16px"
+      width={['90vw', '80vw', '70vw', '55vw']}
+      boxShadow=" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    >
+      <VStack>
+        <Flex direction={"column"} justifyContent={'space-around'}>
+        <Box p={"6px"}  height={['','','','150px']}>
+          <SkeletonText mt='1' noOfLines={3} spacing='5' skeletonHeight='3' w={["60px","80px","120px"]}/>
+        </Box>
+        <HStack>
+        <SkeletonCircle/>
+        <Box>
+          <SkeletonText mt={'1'} noOfLines={2} spacing='3' skeletonHeight='2' w={["35px","55px","75px"]}/>
+        </Box>
+        </HStack>
+        </Flex>
+      </VStack>
+      <Spacer />
 
-          <Box w="100%" textAlign={'center'}>
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
+      <VStack>
+      <Box>
+          <SkeletonText mt={'1'} noOfLines={2} spacing='3' skeletonHeight='2' w={["35px","55px","75px"]}/>
+        </Box>
 
-          </Box>
-          <Box w="100%" textAlign={'center'}>
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
-          </Box>
-          <Box w="100%" textAlign={'center'}>
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
+        <br/>
+      <Box>
+          <SkeletonText mt={'1'} noOfLines={2} spacing='3' skeletonHeight='2' w={["35px","55px","75px"]}/>
+        </Box>
+      </VStack>
+      <Spacer />
 
-          </Box>
-          <Box w="100%" textAlign={'center'}>
-          <SkeletonText mt='4' noOfLines={2} spacing='4' skeletonHeight='2' />
-          </Box>
-          <Box w="100%" textAlign={'center'}>
-          <Skeleton height='40px' />
-          </Box>
-        </SimpleGrid>
-      </Card>
+      <VStack>
+      <Flex direction={"column"} justifyContent={"space-around"} justifyItems={"center"}>
+      <Box>
+          <SkeletonText mt={'1'} noOfLines={2} spacing='3' skeletonHeight='1' w={["35px","55px","75px"]}/>
+      </Box>
+      <Skeleton height={"30px"} width={['60px']} mt={["40px"]}/>
+      </Flex>
+      </VStack>
+    </Flex>
   );
 };
 export default LoadingCard;
